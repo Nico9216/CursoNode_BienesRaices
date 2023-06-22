abac020ef0e266a773b15b95bda476aa559cf240
Nico1692 <nico9216@hotmail.com>
2023-06-22 16:49:18 -0300

    Inicio

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -25,17 +25,32 @@ const registrar= async (req,res)=>{
     await check('nombre').notEmpty().withMessage('El nombre es obligatorio').run(req)
     await check('email').isEmail().withMessage('Formato de email no válido').run(req)
     await check('password').isLength({min:6}).withMessage('El password debe tener al menos 6 caracteres').run(req)
-    await check('repetir_password').equals('password').withMessage('Los passwords no coinciden').run(req)
+    await check('repetir_password').equals(req.body.password).withMessage('Los passwords no coinciden').run(req)
 
 
     let resultado=validationResult(req) //revisa las reglas definidas anteriormente y devolverá un arreglo con los errores en caso de existir
 
-    console.log(resultado.array());
     //Verificar que el resultado es vacío
     if(!resultado.isEmpty()){
         return res.render('auth/registro',{ //Recordar 'pagina', 'errores' son propiedades que yo creo y pueden variar el nombre
             pagina:'Recupera tu acceso a Bienes Raices',
-            errores:resultado.array(),
+            errores:resultado.array(), //Errores es una propiedad que accede registro.pug para mostrar los errores
+            usuario:{
+                nombre:req.body.nombre,
+                email:req.body.email,
+            }
+        })
+    }
+    
+    //Extraer los datos
+    const{nombre,email,password}=req.body
+
+    //Verificar que el usuario no este duplicado (mismo email=duplicado)
+    const existeUsuario=await Usuario.findOne({where:{email}})//{email:email} como tiene el mismo nombre se puede omitir
+    if(existeUsuario){
+        return res.render('auth/registro',{ 
+            pagina:'Recupera tu acceso a Bienes Raices',
+            errores:[{msg:'El usuario ya está registrado'}],
             usuario:{
                 nombre:req.body.nombre,
                 email:req.body.email,
@@ -43,8 +58,14 @@ const registrar= async (req,res)=>{
         })
     }
 
-    const usuario=await Usuario.create(req.body)
-    res.json(usuario)
+    //Almacenar usuario
+    await Usuario.create({
+        nombre,
+        email,
+        password,
+        token:123}
+    )
+    
 }
 
 export{
--- models/Usuario.js
+++ models/Usuario.js
@@ -1,4 +1,5 @@
 import {DataTypes} from 'sequelize';
+import bcrypt from 'bcrypt'
 import db from '../config/db.js'
 
 const Usuario=db.define('usuarios',{ //usuarios sera el nombre de la tabla
@@ -17,6 +18,14 @@ const Usuario=db.define('usuarios',{ //usuarios sera el nombre de la tabla
     token:DataTypes.STRING,
     confirmado:DataTypes.BOOLEAN
 
-})
+},{
+    hooks:{
+        beforeCreate:async function(usuario){ //usuario sería el req.body
+            const salt =await bcrypt.genSalt(10)
+            usuario.password=await bcrypt.hash(usuario.password,salt);
+        }
+    }
+}
+)
 
 export default Usuario
